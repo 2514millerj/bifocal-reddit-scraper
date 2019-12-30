698c8e04fd02f9448ed6a65337aef556f45a67e5
Justin Miller <justin@scanalyticsinc.com>
2019-12-30 17:50:43 -0600

    format submissions and pull out necessary data

--- app.py
+++ app.py
@@ -1,18 +1,51 @@
 import logging
 import praw
+import json
 
-from .config import conservative_subreddits, liberal_subreddits
+from config import conservative_subreddits, liberal_subreddits
+from analysis import extract_keywords
+from database import store_submissions
 
-def get_conservative_posts(reddit):
+def get_submissions(reddit):
+    submissions = []
     for subreddit_name in conservative_subreddits:
-        print(subreddit_name)
+        subreddit = reddit.subreddit(subreddit_name)
+        hot_submissions = subreddit.hot(limit=5)
+        hot_submissions = [('conservative', x) for x in hot_submissions]
+        submissions += hot_submissions
 
-def get_liberal_posts(reddit):
     for subreddit_name in liberal_subreddits:
-        print(subreddit_name)
+        subreddit = reddit.subreddit(subreddit_name)
+        hot_submissions = subreddit.hot(limit=5)
+        hot_submissions = [('liberal', x) for x in hot_submissions]
+        submissions += hot_submissions
+    
+    return submissions
 
-def process_posts(conservative_posts, liberal_posts):
-    pass
+def process_submissions(submissions):
+    formatted_submissions = []
+    for submission in submissions:
+        data = submission[1]
+        label = submission[0]
+
+        if data.permalink not in data.url:
+            keywords = extract_keywords(data.url)
+        else:
+            continue
+
+        formatted_data = {
+            "label": label,
+            "url": data.url,
+            "upvote_ratio": data.upvote_ratio,
+            "title": data.title,
+            "upvotes": data.score,
+            "permalink": data.permalink,
+            "keywords": keywords
+        }
+        formatted_submissions.append(formatted_data)
+
+    return formatted_submissions
+            
 
 def store_headlines(headlines):
     pass
@@ -20,18 +53,13 @@ def store_headlines(headlines):
 if __name__ == "__main__":
     logging.info("Running Bifocal News Reddit Scraper")
 
-    reddit = praw.Reddit(client_id='PERSONAL_USE_SCRIPT_14_CHARS', \
-                     client_secret='SECRET_KEY_27_CHARS ', \
-                     user_agent='YOUR_APP_NAME', \
-                     username='YOUR_REDDIT_USER_NAME', \
-                     password='YOUR_REDDIT_LOGIN_PASSWORD')
+    reddit = praw.Reddit()
 
     #get hottest posts from conservative and liberal subreddits
-    conservative_posts = get_conservative_posts(reddit)
-    liberal_posts = get_liberal_posts(reddit)
+    submissions = get_submissions(reddit)
 
     #pull necessary information from posts and label
-    formatted_headlines = process_posts(conservative_posts, liberal_posts)
+    formatted_submissions = process_submissions(submissions)
 
     #store headlines in database
-    store_headlines(formatted_headlines)
+    store_submissions(formatted_submissions)
